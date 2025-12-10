87506af6e3c012b5247e20358c45f2a98667a65f
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2025-12-10 12:28:23 +0000

    20 mins insufficient (sometimes)

--- data-raw/slurm.sh
+++ data-raw/slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 8            # number of CPU cores, one per thread, up to 128
 #SBATCH --mem=24M       # memory required, up to 250G on standard nodes
 #SBATCH --gres=tmp:40M  # temporary disk space required on the compute node ($TMPDIR), up to 400G
-#SBATCH --time=0:20:0   # time limit for job (format:  days-hours:minutes:seconds)
+#SBATCH --time=0:40:0   # time limit for job (format:  days-hours:minutes:seconds)
 
 # Run in the 'shared' queue (job may share node with other jobs)
 #SBATCH -p shared
