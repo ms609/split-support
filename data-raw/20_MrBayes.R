f334e18b51ce2b54ccf9281f816134ed1bafff46
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2025-12-10 10:54:23 +0000

    Update MB workflow - serial

--- data-raw/20_MrBayes.R
+++ data-raw/20_MrBayes.R
@@ -1,23 +1,26 @@
-source("data-raw/config.R")
+# Set simulation identifier
+sim <- "gam"
+
+source("data-raw/_config.R")
 
 if(!dir.exists("data-raw/alignments")) {
-  source("data-raw/simulate.R")
+  source("data-raw/10_simulate.R")
 }
 
 
-template <- readLines("data-raw/mb.nex")
+template <- readLines(file.path("data-raw", sprintf("mb-%s.nex", sim)))
 
-for (aln in alns) {
+for (aln in alnIDs) {
   
-  if (file.exists(MBFile(aln, "tstat"))) {
+  if (file.exists(MBFile(sim, aln, "tstat"))) {
     message("Tree probabilities found for alignment ", aln)
   } else {
-    on.exit(unlink(MBFile(aln)))
+    on.exit(unlink(MBFile(sim, aln)))
     writeLines(
-      c(readLines(DataFile(aln)), template),
-      MBFile(aln)
+      c(readLines(DataFile(sim, aln)), template),
+      MBFile(sim, aln)
     )
-    system2(mbExec, MBFile(aln))
+    system2(mbExec, MBFile(sim, aln))
     
     
     # Remove unneeded results files
