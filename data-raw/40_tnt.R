dc8407021628c539bb538a2b65dd47bac765d8a8
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2025-12-10 12:15:30 +0000

    Update TNT script

--- data-raw/40_tnt.R
+++ data-raw/40_tnt.R
@@ -1,3 +1,6 @@
+# Set simulation identifier
+sim <- "gam"
+
 source("data-raw/_config.R")
 
 if(!dir.exists("data-raw/alignments")) {
@@ -5,22 +8,22 @@ if(!dir.exists("data-raw/alignments")) {
 }
 
 on.exit(unlink("*.tmp.tre"))
-for (aln in alns) {
-  if (file.exists(TNTFile(aln, "ew"))) {
+for (aln in alnIDs) {
+  if (file.exists(TNTFile(sim, aln, "ew"))) {
     message("Results found for ", aln)
   } else {
     system2(
       tntExec, 
       paste0("run data-raw/tnt-ew.run", " ",
-             DataFile(aln), " ",
-             TNTFile(aln, "ew"), " ;")
+             DataFile(sim, aln), " ",
+             TNTFile(sim, aln, "ew"), " ;")
     )
   }
 }
 
 # Validation step
-for (aln in alns) {
-  thisFile <- TNTFile(aln, "ew")
+for (aln in alnIDs) {
+  thisFile <- TNTFile(sim, aln, "ew")
   if (file.exists(thisFile) && length(readLines(thisFile)) < 60) {
     message("Deleting incomplete analysis: ", aln)
     file.remove(thisFile)
