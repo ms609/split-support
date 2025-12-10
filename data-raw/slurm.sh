ba3761f117c300311ecb0cfca60babf773fc339f
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2025-12-10 12:02:50 +0000

    reduce memory footprint

--- data-raw/slurm.sh
+++ data-raw/slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Request resources:
-#SBATCH -c 8          # number of CPU cores, one per thread, up to 128
-#SBATCH --mem=1G      # memory required, up to 250G on standard nodes
-#SBATCH --gres=tmp:1G # temporary disk space required on the compute node ($TMPDIR), up to 400G
-#SBATCH --time=0:20:0  # time limit for job (format:  days-hours:minutes:seconds)
+#SBATCH -c 8            # number of CPU cores, one per thread, up to 128
+#SBATCH --mem=24M       # memory required, up to 250G on standard nodes
+#SBATCH --gres=tmp:40M  # temporary disk space required on the compute node ($TMPDIR), up to 400G
+#SBATCH --time=0:20:0   # time limit for job (format:  days-hours:minutes:seconds)
 
 # Run in the 'shared' queue (job may share node with other jobs)
 #SBATCH -p shared
