68c3f1cadb729f1953b2f8f272dba520e897bf73
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-12-11 16:23:37 +0000

    Validate character concordance

--- data-raw/60_byChar.R
+++ data-raw/60_byChar.R
@@ -7,14 +7,15 @@ source("data-raw/_config.R")
 
 # Set simulation identifier here
 sim <- "gam"
+cats <- phangorn::discrete.gamma(1, nCats)
 
 referenceTree <- file.path("data-raw", sprintf("reference-%s.tre", sim)) |>
   read.tree()
 refSplits <- as.Splits(referenceTree)
 tips <- names(read.nexus.data(DataFile(sim, "0001")))
 
-concordN <- numeric(0)
-for (i in cli::cli_progress_along(seq_len(nAln), "Analysing")) {
+concordN <- vapply(cli::cli_progress_along(seq_len(nAln), "Analysing"),
+                   function (i) {
   aln <- alnIDs[[i]]
   
   # Calculate concordances
@@ -34,8 +35,8 @@ for (i in cli::cli_progress_along(seq_len(nAln), "Analysing")) {
                                   return = "char")
     write(conc, concCache)
   }
-  concordN <- cbind(concordN, conc)
-}
+  conc
+}, double(nChar))
 
 concN <- data.frame(
   conc = as.vector(concordN),
@@ -48,3 +49,41 @@ boxplot(concN$conc ~ concN$cat, notch = TRUE,
 #plot(conc~log(cat), data = concN)
 summary(lm(conc ~ cat, data = concN))
 summary(lm(conc ~ log(cat), data = concN))
+
+
+concord <- vapply(cli::cli_progress_along(seq_len(nAln), "Analysing"),
+                  function(i) {
+  aln <- alnIDs[[i]]
+
+  # Calculate concordances
+  dataset <- MatrixToPhyDat(matrix(unlist(read.nexus.data(DataFile(sim, aln))),
+                                   nrow = nTip, byrow = TRUE,
+                                   dimnames = list(tips, NULL)))
+
+  concCache <- ConcFile(sim, aln, "_chr")
+  if (file.exists(concCache)) {
+    conc <- scan(concCache, quiet = TRUE)
+    if (length(conc) != nChar) {
+      file.remove(ConcFile(sim, aln))
+      stop("Dimension mismatch; is concordance cache ", aln, " out of date?")
+    }
+  } else {
+    conc <- ClusteringConcordance(refSplits, dataset, normalize = FALSE,
+                                  return = "char")
+    write(conc, concCache)
+  }
+  conc
+}, double(nChar))
+
+
+concDF <- data.frame(
+  conc = as.vector(concord),
+  cat = rep(signif(cats, 4), each = nChar / nCats)
+)
+
+boxplot(concDF$conc ~ concDF$cat, notch = TRUE,
+        frame.plot = FALSE, las = 3,
+        ylab = "Normalized clustering concordance",
+        xlab = "Generative rate")
+summary(lm(conc ~ log(cat), data = concDF))
+summary(lm(log(conc) ~ log(cat), data = concDF))
