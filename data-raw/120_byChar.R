14560744eb78532cb186002016c5490da3bd9c94
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2026-01-14 14:51:14 +0000

    Simplify

--- data-raw/120_byChar.R
+++ data-raw/120_byChar.R
@@ -18,15 +18,14 @@ concord <- vapply(
   cli::cli_progress_along(seq_len(nAln), "Analysing"),
   function(i) {
     aln <- alnIDs[[i]]
-    
+
     # Calculate concordances
     dataset <- DataFile(sim, aln) |>
       read.nexus.data() |>
       unlist() |>
-      matrix(nrow = nTip, byrow = TRUE,
-             dimnames = list(tips, NULL)) |>
+      matrix(nrow = nTip, byrow = TRUE, dimnames = list(tips, NULL)) |>
       MatrixToPhyDat()
-    
+
     concCache <- ConcFile(sim, aln, "_chr")
     if (file.exists(concCache)) {
       conc <- scan(concCache, quiet = TRUE)
@@ -35,8 +34,12 @@ concord <- vapply(
         stop("Dimension mismatch; is concordance cache ", aln, " out of date?")
       }
     } else {
-      conc <- ClusteringConcordance(refSplits, dataset, normalize = FALSE,
-                                    return = "char")
+      conc <- ClusteringConcordance(
+        refSplits,
+        dataset,
+        normalize = FALSE,
+        return = "char"
+      )
       write(conc, concCache)
     }
     conc
@@ -48,15 +51,14 @@ qConcord <- vapply(
   cli::cli_progress_along(seq_len(nAln), "Analysing"),
   function(i) {
     aln <- alnIDs[[i]]
-    
+
     # Calculate concordances
     dataset <- DataFile(sim, aln) |>
       read.nexus.data() |>
       unlist() |>
-      matrix(nrow = nTip, byrow = TRUE,
-             dimnames = list(tips, NULL)) |>
+      matrix(nrow = nTip, byrow = TRUE, dimnames = list(tips, NULL)) |>
       MatrixToPhyDat()
-    
+
     concCache <- ConcFile(sim, aln, "_chrQ")
     if (file.exists(concCache)) {
       conc <- scan(concCache, quiet = TRUE)
@@ -77,20 +79,19 @@ consist <- vapply(
   cli::cli_progress_along(seq_len(nAln), "Analysing"),
   function(i) {
     aln <- alnIDs[[i]]
-    
+
     # Calculate concordances
     dataset <- DataFile(sim, aln) |>
       read.nexus.data() |>
       unlist() |>
-      matrix(nrow = nTip, byrow = TRUE,
-             dimnames = list(tips, NULL)) |>
+      matrix(nrow = nTip, byrow = TRUE, dimnames = list(tips, NULL)) |>
       MatrixToPhyDat()
-    
+
     consCache <- ConcFile(sim, aln, "_cns")
     if (file.exists(consCache)) {
       cons <- do.call(cbind, read.table(consCache))
       if (nrow(cons) != nChar) {
-        # file.remove(ConcFile(sim, aln, "_cns"))
+        file.remove(ConcFile(sim, aln, "_cns"))
         stop("Dimension mismatch; is consistency cache ", aln, " out of date?")
       }
     } else {
@@ -102,6 +103,7 @@ consist <- vapply(
   matrix(double(), nChar, 7)
 )
 
+# Compile statistics into single data.frame for analysis and filtering
 consDF <- data.frame(
   ci = as.vector(consist[, "ci", ]),
   rci = as.vector(consist[, "rci", ]),
@@ -115,40 +117,52 @@ consDF <- data.frame(
 
 nona <- consDF |> na.omit()
 
+# Calculate resolution:
+resolution <- apply(nona, 2, function(x) length(unique(x)))
+resolution[rev(order(resolution)[-1])]
+
+# Plot relationship with generative rate categories
 CIPlot <- function(x, calcTau = FALSE) {
-  boxplot(consDF[[x]] ~ consDF$cat,# notch = TRUE,
-          frame.plot = FALSE, las = 3, range = 1,
-          pch = 16, cex = 0.2,
-          ylab = c(ci = "Consistency index",
-                   ri = "Retention index",
-                   rhi = "1 - Relative homoplasy index",
-                   rhiBar = "1 - Relative homoplasy index (mean)",
-                   conc = "Clustering concordance",
-                   qConc = "Quartet concordance",
-                   rci = "Rescaled consistency index")[[x]],
-          col = switch(x, conc = "gold", NULL),
-          xlab = "")
-  
+  boxplot(
+    consDF[[x]] ~ consDF$cat,
+    frame.plot = FALSE,
+    las = 3,
+    range = 1,
+    pch = 16,
+    cex = 0.2,
+    ylab = c(
+      ci = "Consistency index",
+      ri = "Retention index",
+      rhi = "1 - Relative homoplasy index",
+      rhiBar = "1 - Relative homoplasy index (mean)",
+      conc = "Clustering concordance",
+      qConc = "Quartet concordance",
+      rci = "Rescaled consistency index"
+    )[[x]],
+    col = switch(x, conc = "gold", NULL),
+    xlab = ""
+  )
+
   if (calcTau) {
     cor.test(nona[[x]], as.numeric(nona$cat), method = "kendall")
   }
 }
 
-# Resolution:
-resolution <- apply(nona, 2, function(x) length(unique(x)))
-resolution[rev(order(resolution)[-1])]
-
 {
   pdf("../char-concord/Fig 3 - character concordance.pdf", 8.4, 2.4)
-  par(mfrow = c(1, 6), mar = c(3.7, 5.2, 0.2, 0.2), cex = 0.6,
-      oma = c(1, 0, 0, 0))
-  
-  CIPlot("conc")    # tau ~ -0.6530646
-  CIPlot("qConc")   # tau ~ -0.4571022
-  CIPlot("ci")      # tau ~ -0.7734901
-  CIPlot("ri")      # tau ~ -0.4762535
-  CIPlot("rhi")     # tau ~ -0.5549066
-  CIPlot("rci")     # tau ~ -0.5211873
+  par(
+    mfrow = c(1, 6),
+    mar = c(3.7, 5.2, 0.2, 0.2),
+    cex = 0.6,
+    oma = c(1, 0, 0, 0)
+  )
+
+  CIPlot("conc") # tau ~ -0.6530646
+  CIPlot("qConc") # tau ~ -0.4571022
+  CIPlot("ci") # tau ~ -0.7734901
+  CIPlot("ri") # tau ~ -0.4762535
+  CIPlot("rhi") # tau ~ -0.5549066
+  CIPlot("rci") # tau ~ -0.5211873
   mtext("Generative rate for character", 1, line = 0, outer = TRUE, cex = 0.6)
   dev.off()
 }
